ce110fe8162f0bbd43e8f1cdb4464830b79565f4
Koen Klinkers <k.klinkers@pm.me>
2026-03-15 22:05:37 +0100

    update

--- keymap.c
+++ keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_WORKMAN] = LAYOUT_planck_grid(
     KC_TAB,         KC_Q,    KC_D,    KC_R,    KC_W,             KC_B,              KC_J,    KC_F,    KC_U,    KC_P,    KC_SCLN, KC_BSPC,
     LCTL_T(KC_ESC), KC_A,    KC_S,    KC_H,    KC_T,             KC_G,              KC_Y,    KC_N,    KC_E,    KC_O,    KC_I,    KC_ENT,
-    LSFT_T(KC_DQT), KC_DQT,    KC_X,    KC_M,    KC_C,             KC_V,              KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_QUOT),
+    LSFT_T(KC_DQT), KC_Z,    KC_X,    KC_M,    KC_C,             KC_V,              KC_K,    KC_L,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_QUOT),
     KC_LCTL,        KC_LALT, KC_LGUI, KC_LEFT, LT(RAISE,KC_RIGHT), LT(LOWER, KC_SPC), LGUI_T(KC_BSPC), LT(RAISE, KC_DOWN), KC_UP, KC_LBRC, KC_RBRC, MO(LOWER)
 ),
 
